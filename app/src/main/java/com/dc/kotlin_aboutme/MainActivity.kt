4e6c7ff242b071afa0a92a3ab95078470250931b
Deepak Choudhary <choudharydeepak990@gmail.com>
2020-01-25 04:44:28 +0530

    Update MainActivity.kt

--- app/src/main/java/com/dc/kotlin_aboutme/MainActivity.kt
+++ app/src/main/java/com/dc/kotlin_aboutme/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.dc.kotlin_aboutme
 
 import android.content.Context
+import android.databinding.DataBindingComponent
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,13 +11,18 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import com.dc.kotlin_aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+       binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+       // findViewById<Button>(R.id.done_button).setOnClickListener {
+         //   addNickname(it)
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
     }
@@ -24,10 +31,10 @@ class MainActivity : AppCompatActivity() {
         val editText = findViewById<EditText>(R.id.nick_name_edit_text)
         val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
 
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+        binding.nicknameText.text = binding.nickNameEditText.text
+        binding.nameText.visibility = View.GONE
+        binding.doneButton.visibility = View.GONE
+        binding.nickNameEditText.visibility = View.VISIBLE
 
         // Hide the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
