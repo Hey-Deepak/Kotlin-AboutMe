3428eb27515a01173af406cbb52a8b77ea052f20
Deepak Choudhary <choudharydeepak990@gmail.com>
2020-01-25 04:52:20 +0530

    Update MainActivity.kt

--- app/src/main/java/com/dc/kotlin_aboutme/MainActivity.kt
+++ app/src/main/java/com/dc/kotlin_aboutme/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.dc.kotlin_aboutme
 
 import android.content.Context
-import android.databinding.DataBindingComponent
 import android.databinding.DataBindingUtil
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageView
 import android.widget.TextView
 import com.dc.kotlin_aboutme.databinding.ActivityMainBinding
 
@@ -30,11 +27,12 @@ class MainActivity : AppCompatActivity() {
     private fun addNickname(view: View) {
         val editText = findViewById<EditText>(R.id.nick_name_edit_text)
         val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
-
-        binding.nicknameText.text = binding.nickNameEditText.text
-        binding.nameText.visibility = View.GONE
-        binding.doneButton.visibility = View.GONE
-        binding.nickNameEditText.visibility = View.VISIBLE
+        binding.apply {
+            binding.nicknameText.text = binding.nickNameEditText.text
+            binding.nickNameEditText.visibility = View.GONE
+            binding.doneButton.visibility = View.GONE
+            binding.nicknameText.visibility = View.VISIBLE
+        }
 
         // Hide the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
