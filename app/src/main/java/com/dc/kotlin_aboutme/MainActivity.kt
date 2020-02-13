ab21e89cece4feb1606a112bbb6697943ac4749b
Deepak Choudhary <choudharydeepak990@gmail.com>
2020-02-14 04:00:01 +0530

    Update MainActivity.kt

--- app/src/main/java/com/dc/kotlin_aboutme/MainActivity.kt
+++ app/src/main/java/com/dc/kotlin_aboutme/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.dc.kotlin_aboutme
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
@@ -13,17 +15,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.done_button).setOnClickListener{
+        findViewById<Button>(R.id.done_button).setOnClickListener {
             addNickname(it)
         }
     }
-private fun addNickname(view: View){
-    val editText = findViewById<EditText>(R.id.nick_name_edit_text)
-    val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
 
-    nicknameTextView.text = editText.text
-    editText.visibility = View.GONE
-    view.visibility = View.GONE
-    nicknameTextView.visibility = View.GONE
-}
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nick_name_edit_text)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.GONE
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
 }
